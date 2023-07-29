92293c65bb66012f142867bafdcf0879f9919672
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-07-29 20:34:47 +0700

    Update splash.dart

--- lib/features/intro/presentation/pages/splash.dart
+++ lib/features/intro/presentation/pages/splash.dart
@@ -7,7 +7,7 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 50), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
